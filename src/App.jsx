c6bf84ad509e21ed0f86d256e46ad579aaa9f57c
unknown <https://github.com/Manjit99mk/Vckojtm24.git>
2025-07-01 12:09:09 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,49 @@
+import { Route, BrowserRouter as Router, Routes } from "react-router-dom"
+import HomePage from "./pages/HomePages"
+import AboutPage from "./pages/AboutPage"
+import CoursesPage from "./pages/CoursesPage"
+import ContactPage from "./pages/ContactPage"
+import './styles/Pages.css'
+import Footer from "./components/Footer/Footer"
+import ChatbotComponent from "./components/Chatbot/ChatbotComponents"
+//c:/ojt/DAY 4/VCK firstproject/vck-ojt-app/src/Components/Chatbot/ChatbotComponents
+import AdmissionPage from "./pages/AdmissionPage"
+import { useState } from "react"
+import DeveloperInfoPopup from "./components/Developerinfo/DeveloperInfoPopup"
+const App = () => {
+  const [showPopup, setShowPopup] = useState(true);
+  const handleClosePopup = () => {
+    setShowPopup(false);
+  };
+  return(
+    <>
+      <div>
+        {/* Your main application content */}
+        <DeveloperInfoPopup
+          show={showPopup}
+          onClose={handleClosePopup}
+          studentName="Jitt Kakatkar"
+          studentPhotoUrl="/Images/jitt2.jpg" // Path to their photo
+          uniqueMessage="Learned so much during this OJT! This app showcases my independent coding and deployment skills"
+        />
+      </div>
+    
+      <Router>
+        <Routes>
+          <Route path="/" element={<HomePage/>}/>
+          <Route path="/home" element={<HomePage/>}/>
+          <Route path="/about" element={<AboutPage/>}/>
+          <Route path="/cours" element={<CoursesPage/>}/>
+          <Route path="/contact" element={<ContactPage/>}/>
+          <Route path="/addmission" element={<AdmissionPage/>}/>
+          
+        </Routes>
+         <ChatbotComponent/>
+      
+         
+      </Router>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,23 @@
+import { Link } from "react-router-dom";
+import './Header.css'
+
+const Header = () => {
+  return (
+    <div className="header1">
+    <header className="site-header">
+      <div className="brand">
+       <Link to="/" className="VivClg" style={{color: "#fff", fontSize: "1.7rem",fontWeight: "bold",letterSpacing: "1px", fontFamily: "'Segoe UI', Arial, sans-serif"}}> Vivekanand College</Link>
+        </div>
+      <nav className="nav-links">
+        <Link to="/home" className="Links" style={{color:"white"}}>Home</Link>
+        <Link to="/about" className="Links" style={{color:"white"}}> About</Link>
+        <Link to="/cours" className="Links" style={{color:"white"}}> Courses</Link>
+        <Link to="/contact" className="Links" style={{color:"white"}}> Contact</Link>
+        <Link to="/addmission" className="apply-btn" > Apply Now!</Link>
+      </nav>
+    </header>
+    </div>
+  );
+};
+
+export default Header;
